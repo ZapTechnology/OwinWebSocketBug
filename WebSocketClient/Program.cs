59f1012e46ce9fc7ead8403a1fe6aed263eb38e7
Elad Zelingher <elad@codesharp.net>
2018-01-19 10:02:30 -0500

    ClientWebSocket implementation

--- WebSocketClient/Program.cs
+++ WebSocketClient/Program.cs
@@ -1,32 +1,77 @@
 ﻿using System;
-using WebSocket4Net;
+using System.IO;
+using System.Net.WebSockets;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebSocketClient
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            WebSocket webSocket = new WebSocket("ws://localhost:55167/ws");
+            var webSocket = new ClientWebSocket();
 
+            await webSocket.ConnectAsync(new Uri("ws://localhost:55167/ws"), CancellationToken.None);
+
+            var memoryStream = new MemoryStream();
+
+            await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(0.ToString())), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            await Task.Run(() => HandleSocket(webSocket, new ArraySegment<byte>(new byte[1024]), CancellationToken.None, memoryStream));
+        }
+
+        private static async Task HandleSocket(WebSocket webSocket, ArraySegment<byte> receivedDataBuffer,
+            CancellationToken cancellationToken, MemoryStream memoryStream)
+        {
             int i = 0;
 
-            webSocket.MessageReceived +=
-                (sender, eventArgs) =>
+            StreamReader reader = new StreamReader(memoryStream);
+
+            //Checks WebSocket state.
+            while (webSocket.State == WebSocketState.Open)
+            {
+                await ReadMessage(webSocket, receivedDataBuffer, memoryStream);
+
+                memoryStream.Position = 0;
+
+                string message = reader.ReadToEnd();
+
+                if (message == i.ToString())
                 {
-                    if (eventArgs.Message == i.ToString())
-                    {
-                        Console.WriteLine(i);
-                        i++;
-                        webSocket.Send(i.ToString());
-                    }
-                };
+                    Console.WriteLine(i);
+                    i++;
+                    byte[] buffer = Encoding.UTF8.GetBytes(i.ToString());
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
 
-            webSocket.Opened += (sender, eventArgs) => webSocket.Send(i.ToString());
+                memoryStream.Position = 0;
+                memoryStream.SetLength(0);
+            }
+        }
 
-            webSocket.Open();
+        private static async Task ReadMessage(WebSocket webSocket, ArraySegment<byte> receivedDataBuffer, MemoryStream memoryStream)
+        {
+            WebSocketReceiveResult webSocketReceiveResult;
 
-            Console.ReadLine();
+            long length = 0;
+
+            do
+            {
+                webSocketReceiveResult =
+                    await webSocket.ReceiveAsync(receivedDataBuffer, CancellationToken.None)
+                        .ConfigureAwait(false);
+
+                length += webSocketReceiveResult.Count;
+
+                await memoryStream.WriteAsync(receivedDataBuffer.Array,
+                        receivedDataBuffer.Offset,
+                        webSocketReceiveResult.Count)
+                    .ConfigureAwait(false);
+            }
+            while (!webSocketReceiveResult.EndOfMessage);
         }
     }
 }
